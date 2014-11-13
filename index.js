b499d650d03bc4774634c12d20b9dc52f979b502
Gergely Kovács <gg.kovacs@gmail.com>
2014-11-13 08:47:32 +0100

    gulp-px2rem, package.json dep

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * Requires
+ */
+var util = require('gulp-util');
+var PluginError = util.PluginError;
+var through = require('through2');
+var px2rem = require('node-px2rem');
+
+/**
+ * PLUGIN_NAME
+ * @type {String}
+ */
+var PLUGIN_NAME = 'gulp-px2rem';
+
+/**
+ * Gulp px 2 rem
+ * @param  {Object} options
+ * @param  {Object} postCssOptions
+ * @return {Object}
+ */
+function gulpPx2Rem(options, postCssOptions) {
+    return through.obj(function(file, enc, cb) {
+        if (file.isNull()) {
+            this.push(file);
+            return cb();
+        }
+
+        if (file.isStream()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+            return cb();
+        }
+
+        try {
+            file.contents = new Buffer(px2rem.process(file.contents.toString(), options, postCssOptions));
+        } catch (err) {
+            this.emit('error', new PluginError(PLUGIN_NAME, err));
+        }
+
+        this.push(file);
+        cb();
+    });
+}
+
+module.exports = gulpPx2Rem;
