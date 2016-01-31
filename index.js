80bdfb9870a229b6a3c80c107b7b4e9226a5a920
Kovács Gergely <gg.kovacs@gmail.com>
2016-01-31 11:25:49 +0100

    v1.0.6

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 'use strict';
 
-/**
- * Requires
- */
 var util = require('gulp-util');
 var PluginError = util.PluginError;
 var through = require('through2');
 var px2rem = require('node-px2rem');
 
-/**
- * PLUGIN_NAME
- * @type {String}
- */
 var PLUGIN_NAME = 'gulp-px2rem';
 
-/**
- * Gulp px2rem
- * @param  {Object} options
- * @param  {Object} postCssOptions
- * @return {Object}
- */
 function gulpPx2Rem(options, postCssOptions) {
     return through.obj(function(file, enc, cb) {
         if (file.isNull()) {
